db430395155cb6f86d35f0e0d5da87af83c023a3
Veli-Matti Heino <vekkis@gmail.com.com>
2023-10-02 14:10:59 +0300

    chatti eka versio

--- index.js
+++ index.js
@@ -1,41 +1,84 @@
 'use strict';
 
+require ('dotenv').config();
+const port = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require('socket.io');
-const io = new Server(server);
+const http = require('http').createServer(app);
+const cors = require('cors');
+app.use(cors());
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 
 app.use(express.static('public'));
 
+const rooms = {
+  room1: [],
+  room2: [],
+  room3: []
+};
+
+const users = [];
+
+const deleteUserById = (socketId) => {
+  try {
+    const index = users.findIndex(user => user.id === socketId);
+    console.log('deleting user', users[index]);
+    users.splice(index, 1);
+  } catch (error) {
+    console.warn('deleting user failed', error);
+  }
+};
+
 io.on('connection', (socket) => {
   console.log('user is connected', socket.id);
+  io.emit('user list', users);
+
+  socket.on('join', (username) => {
+    users.push({username: username, id: socket.id});
+    console.log('users connected:', users);
+    io.emit('user list', users);
+  });
 
   socket.on('disconnect', () => {
-    console.log('user disconnected', socket.id);
+    console.log('a user disconnected', socket.id);
+    deleteUserById(socket.id);
+    io.emit('user list', users);
   });
 
   socket.on('join room', (room) => {
+    console.log(`${socket.id} joining room`, room);
     socket.join(room);
+    socket.emit('chat history', rooms[`room${room}`]);
   });
 
   socket.on('chat message', (msg) => {
-    io.to(msg.room).emit('chat message', {
-      message: msg.message,
-      nickname: msg.nickname,
-    });
-    console.log(
-      'chat message in room ' +
-        msg.room +
-        ' by user ' +
-        msg.nickname +
-        ': ' +
-        msg.message
-    );
+    console.log('message by: ' + socket.id, msg);
+
+    const room = `room${msg.room}`;
+    const user = users.find(user => user.id === socket.id);
+
+    const UTCTime = new Date();
+    const LocalTime = new Date(UTCTime.getTime() + 3 * 60 * 60 * 1000);
+    const options = { hour: '2-digit', minute: '2-digit' };
+    const formattedTime = LocalTime.toLocaleTimeString('fi-FI', options);
+
+    if (rooms[room].length >= 5) {
+      rooms[room].shift();
+    }
+    const message = {userId:socket.id, user:user.username, message:msg.message, time:formattedTime}
+    rooms[room].push(message);
+    console.log('testii', rooms);
+  
+    io.emit('chat message', message);
   });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+
+http.listen(port, () => {
+  console.log(`listening on port:${port}`);
 });
