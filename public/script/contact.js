d8f950f89a6db6f5ae68db6618d9b5b62e0f9cf7
Kerttu <80481715+kamuke@users.noreply.github.com>
2023-10-06 21:36:52 +0300

    dialogs

--- public/script/contact.js
+++ public/script/contact.js
@@ -2,6 +2,32 @@ import PUBLIC_KEY from '../emailjs.config.js';
 ('use strict');
 const contactForm = document.getElementById('contactForm');
 
+const closeBtn = document.createElement('button');
+closeBtn.classList.add(
+  'bg-gray-950',
+  'text-gray-100',
+  'rounded-lg',
+  'uppercase',
+  'px-4',
+  'py-2',
+  'text-base'
+);
+closeBtn.innerHTML = 'Close';
+
+const dialogSuccess = document.getElementById('modalSuccess');
+const dialogSuccessParagraph = document.getElementById('modalSuccessParagraph');
+
+dialogSuccess.addEventListener('click', () => {
+  dialogSuccess.close();
+});
+
+const dialogFail = document.getElementById('modalFail');
+const dialogFailParagraph = document.getElementById('modalFailParagraph');
+
+dialogFail.addEventListener('click', () => {
+  dialogFail.close();
+});
+
 const initEmailJS = () => {
   // https://dashboard.emailjs.com/admin/account
   emailjs.init(PUBLIC_KEY);
@@ -12,22 +38,24 @@ initEmailJS();
 window.onload = () => {
   contactForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    // generate a five digit number for the contact_number variable
+    // generate a five digit number for contact number
     document.getElementById('contactNumber').value =
       (Math.random() * 100000) | 0;
     // send form through emailjs
     emailjs.sendForm('metrocine_contact', 'contact_form', contactForm).then(
       () => {
-        console.log('SUCCESS!');
-        // TODO: do nicer modals than alerts
-        alert('Contact send! Thank you!');
+        dialogSuccess.append(closeBtn);
+        dialogSuccessParagraph.innerHTML =
+          'Contact send. Thank you for sending a contact for us!';
+        dialogSuccess.showModal();
         contactForm.reset();
       },
       (error) => {
-        console.log('FAILED...', error);
-        alert(
-          'Something went wrong while sending your contact... Please try again after a while.'
-        );
+        dialogFail.append(closeBtn);
+        dialogFailParagraph.innerHTML =
+          'Something went wrong while sending your contact... Please try again after a while.';
+        dialogFail.showModal();
+        console.log('Contact form sending error:', error);
       }
     );
   });
